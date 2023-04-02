acf12db0423760b88cd12270e375d78b029f6993
Rodrigo <rodrigoml2395@gmail.com>
2023-04-02 06:21:26 -0300

    feat: create transaction

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -32,43 +32,23 @@ export default class OrderModel {
     );
   };
 
-  // eslint-disable-next-line max-lines-per-function
-  // createTransaction = async (
-  //   productId: number, 
-  //   userId: number,
-  //   // productsIds: number[],
-  //   alreadyRegistered: boolean | number | undefined,
-  // ) => {
-  //   try {
-  //   // new Promise((resolve, _reject) => {
-  //   //   this.#connection.query('START TRANSACTION').then(() => this.#connection
-  //   //     .execute<ResultSetHeader>('INSERT INTO Trybesmith.orders (user_id) VALUES (?)', [userId]))
-  //   //     .then(([{ insertId }]) => productsIds.map((productId) => this.#connection
-  //   //       .execute(
-  //   //         'UPDATE Trybesmith.products SET order_id=? WHERE id=?',
-  //   //         [insertId, productId],
-  //   //       ))).then(() => resolve(true))
-  //   //     .catch((_e) => this.#connection.query('ROLLBACK'))
-  //   //     .finally(() => this.#connection.query('COMMIT'));
-  //   // })
-  //     await this.#connection.query('START TRANSACTION');
-  //     let id: any = alreadyRegistered;
-  //     console.log(id, 'entrada');
-  //     if (!alreadyRegistered) {
-  //       const [{ insertId }] = await this.#connection
-  //         .execute<ResultSetHeader>('INSERT INTO Trybesmith.orders (user_id) VALUES (?)', [userId]);
-  //       id = insertId;
-  //     }
-  //     await this.#connection.execute<ResultSetHeader>(
-  //       'UPDATE Trybesmith.products SET order_id=? WHERE id=?',
-  //       [id, productId],
-  //     );
-  //     console.log(id, 'saida');
-  //     await this.#connection.query('COMMIT');
-  //     return { message: '', insertId: id };
-  //   } catch (error: any) {
-  //     await this.#connection.query('ROLLBACK');
-  //     return { message: error.message };
-  //   }
-  // };
+  createTransaction = async (
+    userId: number, 
+    productsIds: number[],
+  ): Promise<{ message: string }> => {
+    const placeholders = Object.keys(productsIds).map((_key) => '?').join(', ');
+    await this.#connection.query('START TRANSACTION');
+    const [{ insertId }] = await this.#connection
+      .execute<ResultSetHeader>('INSERT INTO Trybesmith.orders (user_id) VALUES (?)', [userId]);
+    const [{ affectedRows }] = await this.#connection.execute<ResultSetHeader>(
+      `UPDATE Trybesmith.products SET order_id=? WHERE id IN (${placeholders})`,
+      [insertId, ...productsIds],
+    );
+    if (affectedRows !== productsIds.length) {
+      await this.#connection.query('ROLLBACK');
+      return { message: 'Error has occurred' };
+    } 
+    await this.#connection.query('COMMIT');
+    return { message: '' };
+  };
 }
--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -53,38 +53,18 @@ export default class OrderService {
     return { message: '', status: StatusCodes.CREATED, order: { userId, productsIds } };
   };
 
-  // // eslint-disable-next-line max-lines-per-function
-  // createTransaction = async (order: INewOrder, userId: number): Promise<ICreateOrder> => {
-  //   const { productsIds } = order;
-  //   const id: boolean | number = false;
-  //   const error = this.#validationsInputs.validateNewOrder(order);
-  //   if (error.message) return error;
-  //   // const a = await this.#orderModel.createTransaction(userId, productsIds);
-  //   console.log(a);
-  //   const a = productsIds.map(async (productId, index, arr) => {
-  //     console.log(arr[0]);
-  //     if (index === 0) {
-  //       const { insertId } = await this.#orderModel.createTransaction(productId, userId, id);
-  //       id = insertId;
-  //       console.log(id);
-  //     } else {
-  //       await this.#orderModel.createTransaction(productId, userId, id);
-  //     }
-  //   });
-  //   await Promise.all(a);
-  //   productsIds.forEach(async (productId, index) => {
-  //     if (index === 0) {
-  //       const { insertId } = await this.#orderModel.createTransaction(productId, userId, id);
-  //       id = insertId;
-  //       console.log(id);
-  //     } // else {
-  //     //   await this.#orderModel.createTransaction(productId, userId, id);
-  //     // }
-  //     console.log(id);
-  //   });
-  //   const orderId = await this.#orderModel.createTransaction(userId);
-  //   const isFailed = resolveRegistration.find((affectedRows) => affectedRows === 0);
-  //   if (isFailed) return { status: 500, message: 'Erro has occurred' }; 
-  //   return { message: '', status: StatusCodes.CREATED, order: { userId, productsIds } };
-  // };
+  createTransaction = async (order: INewOrder, userId: number): Promise<ICreateOrder> => {
+    const { productsIds } = order;
+    const error = this.#validationsInputs.validateNewOrder(order);
+    if (error.message) return error;
+    const getProducts = productsIds.map(async (id) => this.#productModel.getById(id));
+    const resolveGetById = await Promise.all(getProducts);
+    const productExist = resolveGetById.findIndex((product) => !product); 
+    if (productExist !== -1) {
+      return { status: StatusCodes.NOT_FOUND, message: `productId[${productExist}] not found` };
+    }
+    const newOrder = await this.#orderModel.createTransaction(userId, productsIds);
+    if (newOrder.message) return { status: 500, message: 'Error has occurred' };
+    return { message: '', status: StatusCodes.CREATED, order: { userId, productsIds } };
+  };
 }
